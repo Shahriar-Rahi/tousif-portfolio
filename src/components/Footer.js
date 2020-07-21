48c46393774ff101bd7e711f52c6475debc66509
Shahriar Rahi <md.alshahriarrahi@gmail.com>
2020-07-21 19:37:36 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://shahriar-rahi.github.io/"> Shahriar Rahi</a>
+         Built with < ❤️ by <a href="https://shahriar-rahi.github.io/"> Shahriar Rahi</a>
         </p>
     </footer>
 )
